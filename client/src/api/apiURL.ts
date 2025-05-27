201344f10ba42c27688663138ebd11001493d8ef
Saad Sayyed <saadsyed950@gmail.com>
2025-05-27 21:48:08 +0530

    Deploy dev 6

--- client/src/api/apiURL.ts
+++ client/src/api/apiURL.ts
@@ -1,5 +1,5 @@
 export const apiURL =
-  import.meta.env.VITE_API_URL || "http://localhost:5000/api";
+  import.meta.env.VITE_API_URL || "https://edtech-1nvm.onrender.com/api";
 
 // https://edtech-1nvm.onrender.com/api
 // http://localhost:5000/api
