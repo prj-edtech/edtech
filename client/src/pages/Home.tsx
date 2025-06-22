d80f0080e6f952a62860bad16acd1d24f6dbcc7a
Saad Syed <saadsyed950@gmail.com>
2025-06-22 14:27:20 +0530

    Login screen fix

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,9 +1,8 @@
 import { useAuth0 } from "@auth0/auth0-react";
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import Navbar from "@/_components/Navbar.tsx";
 import { Button } from "@/components/ui/button";
-import Footer from "@/_components/Footer";
+import { Card, CardContent } from "@/components/ui/card";
 
 const Home = () => {
   const { user, isAuthenticated, logout, loginWithRedirect } = useAuth0();
@@ -20,47 +19,30 @@ const Home = () => {
       {isAuthenticated ? (
         <div>
           <h2>Welcome, {user?.name}</h2>
-          <button
-            onClick={() => logout()}
-            className="px-6 py-2 bg-purple-500 text-white rounded cursor-pointer"
-          >
+          <Button onClick={() => logout()} className="rounded-none">
             Logout
-          </button>
+          </Button>
         </div>
       ) : (
-        <>
-          <Navbar />
-          <div className="flex justify-start items-center w-full flex-col gap-y-6">
-            <div className="flex justify-start items-center w-full flex-col gap-y-8">
-              <h1 className="lg:text-7xl font-outfit capitalize w-[50%] text-center font-semibold lg:mt-20">
-                Your Ideas, Documents & Plans. Unified. Welcome to{" "}
-                <span className="underline">EdTech</span>
-              </h1>
-              <h3 className="lg:text-3xl font-outfit capitalize w-[40%] text-center">
-                EdTech is the connected workspace where better thing happens.
-              </h3>
+        <div className="flex justify-center items-center w-full min-h-screen">
+          <Card>
+            <CardContent className="flex justify-center items-center flex-col lg:gap-y-4">
+              <h4 className="font-semibold font-redhat lg:text-2xl">
+                Authorized Users Only
+              </h4>
+              <p className="font-redhat lg:w-[80%] text-center">
+                This is a secured application and requires user authentication
+                to access its features and services.
+              </p>
               <Button
-                className="px-8 py-2 cursor-pointer rounded text-lg"
+                className="rounded-none"
                 onClick={() => loginWithRedirect()}
               >
                 Log in
               </Button>
-              <div className="flex justify-center items-center w-full lg:gap-x-20 lg:mt-6">
-                <img
-                  src="/images/home/img1.png"
-                  alt="Image"
-                  className="max-w-[380px] max-h-[380px]"
-                />
-                <img
-                  src="/images/home/img2.png"
-                  alt="Image"
-                  className="max-w-[380px] max-h-[380px]"
-                />
-              </div>
-            </div>
-          </div>
-          <Footer />
-        </>
+            </CardContent>
+          </Card>
+        </div>
       )}
     </>
   );
