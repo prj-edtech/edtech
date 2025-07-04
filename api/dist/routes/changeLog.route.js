30b7448bacfc97abf36abab1a39e0acba2421179
Saad Sayyed <saadsyed950@gmail.com>
2025-07-04 10:43:39 +0530

    Working on mobile responsive

--- api/dist/routes/changeLog.route.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const changeLog_controller_1 = require("../controllers/changeLog.controller");
-const changeLogRouter = (0, express_1.Router)();
-changeLogRouter.get("/", changeLog_controller_1.getAllChangeLogs);
-changeLogRouter.delete("/", changeLog_controller_1.deleteChangeLogs);
-exports.default = changeLogRouter;
