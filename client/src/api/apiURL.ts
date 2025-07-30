6d975f96341ed1dbb8e7db546e63d2d0c12baf33
Saad Sayyed <saadsyed950@gmail.com>
2025-07-30 13:17:17 +0530

    Deploying as phone-to-check

--- client/src/api/apiURL.ts
+++ client/src/api/apiURL.ts
@@ -1,4 +1,4 @@
-export const apiURL = import.meta.env.VITE_API_URL_DEV!;
+export const apiURL = import.meta.env.VITE_API_URL_PROD!;
 
 // https://edtech-1nvm.onrender.com/api
 // http://localhost:5000/api
